ba6ecdc44fd09d9e080aa5171ca14bfb65aab990
steviedc87 <steven_decock@hotmail.com>
2020-03-25 02:02:01 +0100

    Updates

---
+++ precache-manifest.2581c06d05742fa7bb04b10e5e1fe57c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6494e373ae081c414113f9f8fbbe7801",
+    "url": "/sound-app/index.html"
+  },
+  {
+    "revision": "b052f89499cd5ea7550b",
+    "url": "/sound-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "db8d05e5a60d392552b4",
+    "url": "/sound-app/static/css/main.83e00217.chunk.css"
+  },
+  {
+    "revision": "b052f89499cd5ea7550b",
+    "url": "/sound-app/static/js/2.822e9b0d.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/sound-app/static/js/2.822e9b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8d05e5a60d392552b4",
+    "url": "/sound-app/static/js/main.a17cc621.chunk.js"
+  },
+  {
+    "revision": "cdcd6a6d1485ec721c33",
+    "url": "/sound-app/static/js/runtime-main.e5b14d29.js"
+  }
+]);
